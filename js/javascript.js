c34a005aa053e776e648d5deb3797f7160d28df0
inkbkch <inkbongkoch@gmail.com>
2021-06-16 17:02:02 +0700

    test

---
+++ js/javascript.js
@@ -0,0 +1,68 @@
+var data = [
+    {
+        "id": 1,
+        "stock": true,
+        "rating": 5,
+        "review": 12,
+        "name": "product1",
+        "bfdiscount": "1700.00",
+        "price": "1550.00"
+    },
+    {
+        "id": 2,
+        "stock": false,
+        "rating": 4,
+        "review": 12,
+        "name": "product2",
+        "bfdiscount": "1700.00",
+        "price": "1900.00"
+    },
+    {
+        "id": 3,
+        "stock": true,
+        "rating": 3,
+        "review": 12,
+        "name": "product3",
+        "bfdiscount": "1700.00",
+        "price": "700.00"
+    },
+    {
+        "id": 4,
+        "stock": true,
+        "rating": 5,
+        "review": 12,
+        "name": "product4",
+        "bfdiscount": "1700.00",
+        "price": "1000.00"
+    },
+    {
+        "id": 5,
+        "stock": true,
+        "rating": 5,
+        "review": 12,
+        "name": "product5",
+        "bfdiscount": "1700.00",
+        "price": "1200.00"
+    }
+    ,
+    {
+        "id": 6,
+        "stock": true,
+        "rating": 5,
+        "review": 12,
+        "name": "product6",
+        "bfdiscount": "1700.00",
+        "price": "1200.00"
+    }
+    ,
+    {
+        "id": 7,
+        "stock": true,
+        "rating": 5,
+        "review": 12,
+        "name": "product7",
+        "bfdiscount": "1700.00",
+        "price": "1200.00"
+    }
+
+]
